b5df50fd9644b9ad689f2afd38b2b841910d4367
halidazainon-dev <halidazainon@gmail.com>
2025-03-15 00:39:49 +0800

    feat: Implement listAll method in TaskDAO

--- src/main/java/com/application/todolist/dao/TaskDAO.java
+++ src/main/java/com/application/todolist/dao/TaskDAO.java
@@ -1,6 +1,35 @@
 package com.application.todolist.dao;
 
+import java.lang.reflect.Parameter;
+import java.util.List;
 
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.application.todolist.vo.Task;
+
+@Repository
 public class TaskDAO {
 
+    private final NamedParameterJdbcTemplate jdbcTemplate;
+
+    public TaskDAO(NamedParameterJdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Task> listAll(){
+        String sql = "SELECT * FROM tasks";
+        return jdbcTemplate.query(sql, taskRowMapper);
+    }
+
+    private final RowMapper<Task> taskRowMapper = (rs, rowNum) -> {
+        Task task = new Task();
+        task.setId(rs.getInt("id"));
+        task.setDescription(rs.getString("description"));
+        task.setCompleted(rs.getBoolean("completed"));
+        task.setCreatedDate(rs.getDate("created_date"));
+        return task;
+    };
 }
