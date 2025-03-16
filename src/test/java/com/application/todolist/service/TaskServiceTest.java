15dd754e7b2d85e8ea11b418e17915dbd804122c
halidazainon-dev <halidazainon@gmail.com>
2025-03-16 23:44:35 +0800

    test: Add unit test for home redirection in TaskService

--- src/test/java/com/application/todolist/service/TaskServiceTest.java
+++ src/test/java/com/application/todolist/service/TaskServiceTest.java
@@ -1,5 +1,29 @@
 package com.application.todolist.service;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.application.todolist.dao.TaskDAO;
+
+@WebMvcTest(TaskService.class)
 public class TaskServiceTest {
+
+    @Autowired
+    private MockMvc mockMvc; 
+
+    @MockBean
+    private TaskDAO taskDAO; 
+
+    @Test
+    public void testHomeRedirect() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))   
+                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())  
+                .andExpect(MockMvcResultMatchers.redirectedUrl("/tasks")); 
+    }
 
 }
